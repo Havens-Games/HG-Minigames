cc35efebc5b1b3d13f79ba6aa4829f3f22c99bae
TheDudeFromCI <thedudefromci@gmail.com>
2021-08-05 01:10:05 -0700

    Removed unused iterator interface.

--- src/main/java/net/whg/minigames/framework/iterator/Iterator.java
+++
@@ -1,8 +0,0 @@
-package net.whg.minigames.framework.iterator;
-
-/**
- * Iterates over a collection of objects over the spam of multiple frames.
- */
-public interface Iterator {
-    boolean update();
-}
